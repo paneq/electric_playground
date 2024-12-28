a5d0f18aa7500bdb2a07421fcaa62a019c17b958
Robert Pankowecki <robert.pankowecki@gmail.com>
2024-12-28 07:23:34 +0100

    proxy frontend as well

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/frontend/',
   server: {
     port: 5000,
     open: true
